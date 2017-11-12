4ffdd837869d44cb0f3f10f74d6eb00718a5ba40
Larry Wei <lwei@Larrys-MacBook-Pro.local>
2017-11-12 12:55:12 -0800

    initial commit

---
+++ app/src/main/java/thelarrywei/com/instameme/core/MainActivity.java
@@ -0,0 +1,20 @@
+package thelarrywei.com.instameme.core;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lwei on 11/12/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
